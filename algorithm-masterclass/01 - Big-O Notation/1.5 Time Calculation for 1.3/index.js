1b9b2079219b74d8d5caf1b4a0bc31b3b16a75f2
Tanish Raj <learn.tanishraj@gmail.com>
2025-01-23 18:58:51 +0530

    feat: time calculation for 1.3

---
+++ algorithm-masterclass/01 - Big-O Notation/1.5 Time Calculation for 1.3/index.js
@@ -0,0 +1,25 @@
+/*
+==========================================================================
+Write a function that calculates the sum of all numbers from 1 upto
+ (and including) some other number n.
+
+ 1 to 3 => 1+2+3 => 6
+ 1 to 5 => 1+2+3+4+5 => 15
+==========================================================================
+*/
+
+function addUpTo(num){
+    let total = 0;
+    for(let i = 1;i <= num; i++){
+        total += i;
+    }
+
+    return total;
+}
+
+
+// Timing function
+var t1 = performance.now();
+addUpTo(1000000000);
+var t2 = performance.now();
+console.log(`Time elapsed: ${(t2 - t1) / 1000} seconds.`);
