809f8b5de5c3a480e92d56ffd8757e86778853e3
Tanish Raj <learn.tanishraj@gmail.com>
2025-01-25 12:40:03 +0530

    feat: Array Builting methods time complexity

---
+++ algorithm-masterclass/01 - Big-O Notation/1.8 Array Built in methods Time Cmplexity/index.js
@@ -0,0 +1,17 @@
+/*
+BIG O of Array Operations
+================================================================
+
+push => O(1);
+pop => O(1);
+shift => O(N);
+unshift => O(N);
+unshift => O(N);
+concat => O(N);
+slice => O(N);
+splice => O(N);
+sort => O(N * log N);
+forEach/map/filter/reduce/etc... => o(N)
+
+================================================================
+*/
