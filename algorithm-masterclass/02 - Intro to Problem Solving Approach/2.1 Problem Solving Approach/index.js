f4fe27f8d61f831bb3de34163d5488829eeeaaeb
Tanish Raj <learn.tanishraj@gmail.com>
2025-01-26 22:35:04 +0530

    feat: problem solving

---
+++ algorithm-masterclass/02 - Intro to Problem Solving Approach/2.1 Problem Solving Approach/index.js
@@ -0,0 +1,30 @@
+/*
+================================================================
+
+WHAT IS AN ALGORITHM?
+-----------------------
+A process or set of steps to accomplish a certain task.
+
+
+WHY DO I NEED TO KNOW THIS?
+------------------------------
+Almost everything that you do in programming invloves some 
+kind of algorithms.
+
+
+HOW DO I IMPROVE?
+-------------------
+a) Devise a plan for sloving problems.
+b) Master common problem solving patterns
+
+
+What are all the problem solving approach?
+--------------------------------------------
+a) Understand the problem
+b) Explore concrete examples
+c) Break it down.
+d) Solve or simplify
+e) Look back and refactor
+
+================================================================
+*/
