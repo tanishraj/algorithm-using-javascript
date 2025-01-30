b967bdd47fb141d2e24f745580a5414390d3e1be
Tanish Raj <learn.tanishraj@gmail.com>
2025-01-30 12:05:48 +0530

    feat: Frequency counter pattern

---
+++ algorithm-masterclass/04 - Frequency Counters Pattern/4.1 Naive Solution/index.js
@@ -0,0 +1,42 @@
+/*
+================================================================
+
+Frequency Counters:
+----------------------
+This pattern uses objects or sets to collect values/frequencies 
+of values.
+This can often avoid the need for nested loop or O(n^2) 
+operations with arrays and strings
+
+
+EXAMPLE:
+-----------
+Write a function called same, which accepts two arrays. The 
+function should return true if every value in the array 
+has it's corresponding value squared in the scecond array. 
+The frequency of values must be the same.
+
+same([1,2,3], [4,1,9]); // true
+same([1,2,3], [1,9]); // false
+same([1,2,1], [4,4,1]); // false
+
+================================================================
+*/
+
+
+function same(arr1, arr2){
+    if(arr1.length != arr2.length){
+        return false;
+    }
+
+    for(let i=0; i<arr1.length; i++){
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1){
+            return false;
+        }
+        arr2.splice(correctIndex, 1);
+    }
+    return true;
+}
+
+console.log(same([1,2,3], [1,4,9]));
